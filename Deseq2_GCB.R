6b0844f00b2468dc6651b7ba94e4ddb23bbfd1c4
MikeYuliang <50691918+MikeYuliang@users.noreply.github.com>
2022-03-18 13:27:37 +0800

    Add files via upload

---
+++ Deseq2_GCB.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(DESeq2)
+#import data
+setwd("../GCB/counts")
+mycounts<-read.table("Mergedata.txt",header = TRUE,row.names = 1,sep ="\t" )
+condition<-factor(c(rep("KO",2),rep("WT",2)),levels = c("WT","KO"))
+colData<-data.frame(row.names = colnames(mycounts),condition)
+
+dds <- DESeqDataSetFromMatrix(mycounts, colData, design= ~ condition)
+dds <- DESeq(dds)
+
+res= results(dds)
+res = res[order(res$pvalue),]
+head(res)
+summary(res)
+write.csv(res,file="All_results.csv")
+
